da881a9d51f30f17b515ca304d199fe84438236c
acatejr <acatejr@gmail.com>
2025-01-06 16:02:26 -0700

    Test deployment.

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <>
       <div class='container'>
-        <h1 class='text-5xl font-bold underline'>Hello world!</h1>
+        <h1 class='text-2xl font-bold underline'>Hello world!</h1>
       </div>
     </>
   )
